e2946a2aa46ab86026e5a78a28613de44bec3427
Huancheng Yang <peteryanghc1996@gmail.com>
2016-11-04 17:00:05 -0700

    Added serial communication feature

--- Zaber-script/Port_communication.bat
+++ Zaber-script/Port_communication.bat
@@ -6,6 +6,6 @@ cd Program Files (x86)
 cd Zaber Technologies
 cd Zaber Console 2.0.10
 cd Scripts
-ScriptRunner /port COM3 /m a 3axis3.js >> 233.txt
+ScriptRunner /port COM3 /m a Port_communication.cs>> 233.txt
 
 pause
